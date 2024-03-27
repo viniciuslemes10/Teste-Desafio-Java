c5c0df003eea50b719a664018a832b7b700f0dff
vinicuslemes10 <vinikjhgfds@gmail.com>
2024-03-27 15:45:03 -0300

    fast: Atualizando ClienteRepository

--- src/main/java/com/tigd/api/repository/ClienteRepository.java
+++ src/main/java/com/tigd/api/repository/ClienteRepository.java
@@ -2,6 +2,8 @@ package com.tigd.api.repository;
 
 import com.tigd.api.domain.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
