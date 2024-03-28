64f9ce72135f6a856e71396dce8867984987c18b
vinicuslemes10 <vinikjhgfds@gmail.com>
2024-03-28 19:43:47 -0300

    Refatorando as classes

--- src/main/java/com/tigd/api/validators/CnpjValidator.java
+++ src/main/java/com/tigd/api/validators/CnpjValidator.java
@@ -1,34 +1,45 @@
 package com.tigd.api.validators;
 
+import com.tigd.api.exceptions.CnpjIllegalArgException;
 import org.springframework.stereotype.Component;
 
 @Component
-public class CnpjValidator {
-    protected boolean validarDigito(int dac, int posicao, String typeDocment) {
-        int digito = Character.getNumericValue(typeDocment.charAt(posicao));
-        if(dac == digito) {
-            return true;
-        }
-        return false;
+public class CnpjValidator extends CpfValidator {
+
+    protected boolean validar(String typeDocument, int indice, int posicao) {
+        int soma = percorrendoDocumento(typeDocument, indice);
+        return validarDigito(soma, posicao, typeDocument);
     }
 
-
-    protected int calcularDac(int sum) {
-        int resto = sum % 11;
-        int dac = (resto < 2) ? 0 : (11 - resto);
-        return dac;
-    }
-
-    protected int percorrendoDocumento(String typeDocument, int sum, int peso, int tamanhoIndice) {
-        for(int i = tamanhoIndice; i >= 0; i--) {
-            int num = Character.getNumericValue(typeDocument.charAt(i));
-            sum += num * peso;
+    protected int percorrendoDocumento(String typeDocument, int tamanhoIndice) {
+        int sum = 0;
+        int peso = 2;
+        for (int i = tamanhoIndice; i >= 0; i--) {
+            sum += Character.getNumericValue(typeDocument.charAt(i)) * peso;
             peso++;
-            if(peso == 10) {
+            if (peso == 10) {
                 peso = 2;
             }
         }
         return sum;
     }
 
+    protected boolean validarDigito(int sum, int posicao, String typeDocument) {
+        int digito = Character.getNumericValue(typeDocument.charAt(posicao));
+        return calcularDac(sum) == digito;
+    }
+
+    private int calcularDac(int sum) {
+        int resto = sum % 11;
+        int dac = (resto < 2) ? 0 : (11 - resto);
+        return dac;
+    }
+
+    public boolean validarDacCnpj(boolean dacOne, boolean dacTwo) {
+        if (dacOne && dacTwo) {
+            return true;
+        } else {
+            throw new CnpjIllegalArgException();
+        }
+    }
 }
