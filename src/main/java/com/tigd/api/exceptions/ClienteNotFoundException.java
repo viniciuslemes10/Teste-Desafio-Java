42ece83c972c2a18007ecaa5eafd8ad023a1d5c2
gemeoslemes <victorlemes0776@gmail.com>
2024-03-29 16:50:58 -0300

    JavaDoc de ClienteNotFoundException

--- src/main/java/com/tigd/api/exceptions/ClienteNotFoundException.java
+++ src/main/java/com/tigd/api/exceptions/ClienteNotFoundException.java
@@ -1,11 +1,25 @@
 package com.tigd.api.exceptions;
 
 import java.util.NoSuchElementException;
-
+/**
+ * @author viniciuslemes10, gemeoslemes
+ * Exceção lançada quando cliente não pode ser encontrado.
+ **/
 public class ClienteNotFoundException extends NoSuchElementException {
+
+    /**
+     * Construtor padrão de criar a instância de ClienteNotFaundException
+     * com a menssagem "Cliente não encontrado!".
+     **/
     public ClienteNotFoundException() {
         super("Cliente não encontrado!");
     }
+
+    /**
+     * Construtor que cria a instância ClienteNotFoundException
+     * com a mensagem personalidaza.
+     * @param message A menssagem de erro
+     **/
     public ClienteNotFoundException(String message) {
         super(message);
     }
