6e2e86e191fcc21d1c65d564675d8304311da546
gemeoslemes <victorlemes0776@gmail.com>
2024-04-01 17:35:17 -0300

    Documentação nas classes de documentos com JavaDoc

--- src/main/java/com/tigd/api/validators/CnpjValidator.java
+++ src/main/java/com/tigd/api/validators/CnpjValidator.java
@@ -3,22 +3,54 @@ package com.tigd.api.validators;
 import com.tigd.api.exceptions.CnpjIllegalArgException;
 import org.springframework.stereotype.Component;
 
+/**
+ * @author viniciuslemes10, gemeoslemes<br>
+ * Classe de validação de CNPJ(Cadastro Nacional de Pessoa Júridica).
+ * Estende da classe {@link CpfValidator} para reutilizar funcionalidades de validação.
+ **/
 @Component
 public class CnpjValidator extends CpfValidator {
 
-    protected boolean validarCNPJSegundoDigito(String typeDocument) {
-        int result = calcularSomaCNPJ(typeDocument, 12);
-        return compararDigitoVerificador(result, 13, typeDocument);
-    }
-    protected boolean validarCNPJPrimeiroDigito(String typeDocument) {
-        int result = calcularSomaCNPJ(typeDocument, 11);
-        return compararDigitoVerificador(result, 12, typeDocument);
+    /**
+     * Valida o segundo digito verificador de um CNPJ.
+     * @param documentCnpj O CNPJ a ser validado, com uma sequência de número.<br>
+     * {@link #calcularSomaCNPJ} passando o documento(CNPJ) e o índice que será verificado.
+     * @return {@link #compararDigitoVerificador} comparando o resultado do método {@link #calcularSomaCNPJ},
+     * e a posição do último digito verificador(13ª) e o cnpj passado.
+     **/
+    protected boolean validarCNPJSegundoDigito(String documentCnpj) {
+        int result = calcularSomaCNPJ(documentCnpj, 12);
+        return compararDigitoVerificador(result, 13, documentCnpj);
     }
 
+    /**
+     * Valida o primeiro digito verificador de um CNPJ.
+     * @param documentCnpj O CNPJ a ser validado, com uma sequência de número.<br>
+     * {@link #calcularSomaCNPJ} passando o documento(CNPJ) e o índice que será verificado.
+     * @return {@link #compararDigitoVerificador} comparando o resultado do método {@link #calcularSomaCNPJ},
+     * e a posição do último digito verificador(12ª) e o cnpj passado.
+     **/
+    protected boolean validarCNPJPrimeiroDigito(String documentCnpj) {
+        int result = calcularSomaCNPJ(documentCnpj, 11);
+        return compararDigitoVerificador(result, 12, documentCnpj);
+    }
+    /**
+     * Calcula a soma ponderada dos digítos de um CNPJ até determinada posição.
+     * @param document O CNPJ a ser utilizado para o cálculo da soma.
+     * @param tamanhoDoIndice O índice até o qual os dígitos devem ser considerados na soma.
+     * @return A soma ponderada dos digitos do CNPJ até a posição especificada.
+     **/
     private int calcularSomaCNPJ(String document, int tamanhoDoIndice) {
         return somarValoresDocumento(document, tamanhoDoIndice);
     }
 
+    /**
+     * Soma ponderadamente dos valores dos digítos de um documento até a posição específicada.
+     * @param typeDocument O Documento que será validado.
+     * @param tamanhoIndice O índice até qual os dígitos devem ser considerados na soma.
+     * @return A soma ponderada dos valores dos dígitos do documento até a posição do índice passado como
+     * parâmentro.
+     **/
     protected int somarValoresDocumento(String typeDocument, int tamanhoIndice) {
         int sum = 0;
         int peso = 2;
@@ -32,17 +64,39 @@ public class CnpjValidator extends CpfValidator {
         return sum;
     }
 
+    /**
+     * Compara o dígito verificador calculado com o dígito verificador fornecido em uma posição específica do documento.
+     *
+     * @param resultadoDaSoma O resultado da soma ponderada dos valores dos dígitos do documento.
+     * @param posicao A posição do dígito verificador no documento.
+     * @param document O documento do qual o dígito verificador será comparado, representado como uma sequência de números.
+     * @return true se o dígito verificador calculado for igual ao dígito verificador fornecido, false caso contrário.
+     */
     protected boolean compararDigitoVerificador(int resultadoDaSoma, int posicao, String document) {
         int digito = Character.getNumericValue(document.charAt(posicao));
         return calcularDacDeVerificação(resultadoDaSoma) == digito;
     }
 
+    /**
+     * Calcula o dígito de verificação de um número, utilizando o algoritmo módulo 11.
+     *
+     * @param sum A soma ponderada dos valores dos dígitos do documento.
+     * @return O dígito de verificação calculado.
+     */
     private int calcularDacDeVerificação(int sum) {
         int resto = sum % 11;
         int dac = (resto < 2) ? 0 : (11 - resto);
         return dac;
     }
 
+    /**
+     * Confirma a validade dos dígitos de verificação de um CNPJ.
+     *
+     * @param dacOne O valor booleano que indica se o primeiro dígito de verificação é válido.
+     * @param dacTwo O valor booleano que indica se o segundo dígito de verificação é válido.
+     * @return true se ambos os dígitos de verificação forem válidos, caso contrário, lança uma exceção.
+     * @throws CnpjIllegalArgException Se algum dos dígitos de verificação não for válido.
+     */
     public boolean confirmarCnpjDacs(boolean dacOne, boolean dacTwo) {
         if (dacOne && dacTwo) {
             return true;
--- src/main/java/com/tigd/api/validators/CpfValidator.java
+++ src/main/java/com/tigd/api/validators/CpfValidator.java
@@ -5,12 +5,31 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class CpfValidator {
+
+    /**
+     * Valida um dígito verificador de CPF em um documento, dado os índices da soma ponderada e do dígito.
+     *
+     * @param typeDocument O CPF a ser validado, representado como uma sequência de números.
+     * @param indiceSoma O índice inicial para a soma ponderada dos dígitos.
+     * @param indiceDigito O índice do dígito verificador no documento.
+     * @return true se o dígito verificador do CPF for válido, false caso contrário.<br>
+     * {@link #calcularSomaPonderada(String, int, int, int)}
+     * {@link #validarDigitoVerificadorCpfPorIndice(int, String, int)}
+     */
     protected boolean validarDigitoVerificadorCpf(String typeDocument, int indiceSoma, int indiceDigito) {
         int sum = 0;
         sum = calcularSomaPonderada(typeDocument, sum, indiceSoma, indiceDigito);
         return validarDigitoVerificadorCpfPorIndice(sum, typeDocument, indiceSoma);
     }
 
+    /**
+     * Valida os dígitos verificadores de um CPF.
+     *
+     * @param dacOne O valor booleano que indica se o primeiro dígito verificador é válido.
+     * @param dacTwo O valor booleano que indica se o segundo dígito verificador é válido.
+     * @return true se ambos os dígitos verificadores forem válidos; lança uma exceção CpfIllegalArgException caso contrário.
+     * @throws CpfIllegalArgException se algum dos dígitos verificadores não for válido. Mostrando a menssagem e o status.
+     */
     protected boolean validarDacCpf(boolean dacOne, boolean dacTwo) {
         if(dacOne && dacTwo) {
             return true;
@@ -18,18 +37,37 @@ public class CpfValidator {
             throw new CpfIllegalArgException();
         }
     }
-
+    /**
+     * Verifica se todos os dígitos de um CPF são iguais.
+     *
+     * @param document O CPF a ser verificado, representado como uma sequência de números.
+     * @throws CpfIllegalArgException Se todos os dígitos do CPF forem iguais.
+     */
     protected void isDigitosIguais(String document) {
         if (areTheSameDigits(document)) {
             throw new CpfIllegalArgException();
         }
     }
 
+    /**
+     * Valida um dígito verificador de CPF em uma posição específica do documento, dado o resultado da soma ponderada.
+     *
+     * @param sum A soma ponderada dos valores dos dígitos do documento.
+     * @param typeDocument O CPF a ser validado, representado como uma sequência de números.
+     * @param posicaoDigitoValidacao A posição do dígito verificador no documento.
+     * @return true se o dígito verificador do CPF na posição especificada for válido, false caso contrário.
+     */
     private boolean validarDigitoVerificadorCpfPorIndice(int sum, String typeDocument, int posicaoDigitoValidacao) {
         int digitoCalculado = calcularDigitoVerificador(sum);
         return Character.getNumericValue(typeDocument.charAt(posicaoDigitoValidacao)) == digitoCalculado;
     }
 
+    /**
+     * Calcula o dígito verificador de um número utilizando o algoritmo módulo 11.
+     *
+     * @param sum A soma ponderada dos valores dos dígitos do documento.
+     * @return O dígito verificador calculado.
+     */
     private int calcularDigitoVerificador(int sum) {
         int resto = 11 - (sum % 11);
         if(resto > 9) {
@@ -38,6 +76,15 @@ public class CpfValidator {
         return resto;
     }
 
+    /**
+     * Calcula a soma ponderada dos valores dos dígitos de um documento até uma determinada posição.
+     *
+     * @param typeDocument O documento do qual os valores dos dígitos serão somados, representado como uma sequência de números.
+     * @param sum A soma parcial a ser acumulada durante o cálculo.
+     * @param tamanhoDoFor O tamanho do loop for, determinando quantos dígitos do documento serão considerados na soma.
+     * @param indice O índice do dígito verificador no documento.
+     * @return A soma ponderada dos valores dos dígitos do documento até a posição especificada.
+     */
     private int calcularSomaPonderada(String typeDocument, int sum, int tamanhoDoFor, int indice) {
         for(int i = 0; i < tamanhoDoFor; i++) {
             sum += Character.getNumericValue(typeDocument.charAt(i)) * (indice-i);
@@ -45,11 +92,25 @@ public class CpfValidator {
         return sum;
     }
 
+    /**
+     * Verifica se todos os dígitos de um documento são iguais.
+     *
+     * @param typeDocument O documento a ser verificado, representado como uma sequência de caracteres.
+     * @return true se todos os dígitos do documento forem iguais; false caso contrário.
+     * @see #percorrendoDocumento(String, char)
+     */
     private boolean areTheSameDigits(String typeDocument) {
         char primeiroDigito = typeDocument.charAt(0);
         return percorrendoDocumento(typeDocument, primeiroDigito);
     }
 
+    /**
+     * Verifica se todos os dígitos de um documento são iguais ao primeiro dígito especificado.
+     *
+     * @param document O documento a ser verificado, representado como uma sequência de caracteres.
+     * @param digito O dígito de referência para comparação.
+     * @return true se todos os dígitos do documento forem iguais ao dígito de referência; false caso contrário.
+     */
     private boolean percorrendoDocumento(String document, char digito) {
         for (int i = 1; i < document.length(); i++) {
             if (document.charAt(i) != digito) {
