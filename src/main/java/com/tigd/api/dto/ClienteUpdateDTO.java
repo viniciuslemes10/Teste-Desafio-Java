f113ce40be790a5a1e9e92c9f5edb417cd28b7c5
gemeoslemes <victorlemes0776@gmail.com>
2024-04-02 18:38:21 -0300

    Documentação dos DTO

--- src/main/java/com/tigd/api/dto/ClienteUpdateDTO.java
+++ src/main/java/com/tigd/api/dto/ClienteUpdateDTO.java
@@ -1,5 +1,14 @@
 package com.tigd.api.dto;
 
+/**
+ * @author gemeoslemes, vinicius lemes<br>
+ *
+ * <p>
+ *  Um DTO (Data Transfer Object) que representa as informações que podem ser atualizar de um cliente.
+ * </p>
+ * @param nome O nome do Cliente.
+ * @param email O email do Cliente.
+ **/
 public record ClienteUpdateDTO(
         String nome,
         String email
