54191adb7ac35746884bdbe17b2e6fe5b171687f
vinicuslemes10 <vinikjhgfds@gmail.com>
2024-04-04 18:44:42 -0300

    Criando a classe TestAuxiliaresClienteService.java para reutilização de métodos para testes em ClienteService.

--- src/test/java/com/tigd/api/service/ClienteServiceTest.java
+++ src/test/java/com/tigd/api/service/ClienteServiceTest.java
@@ -5,8 +5,6 @@ import com.tigd.api.exceptions.CpfUniqueException;
 import com.tigd.api.exceptions.EmailUniqueException;
 import com.tigd.api.repository.ClienteRepository;
 import com.tigd.api.validators.DocumentValidator;
-import com.tigd.api.validators.ValidadorAtualizadorEntidade;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -18,125 +16,71 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import java.math.BigDecimal;
 
-import static org.mockito.Mockito.*;
-
-class ClienteServiceTest {
+class ClienteServiceTest  {
     @Mock
     private ClienteRepository clienteRepository;
 
     @Mock
     private DocumentValidator documentValidator;
 
-    @Mock
-    private ValidadorAtualizadorEntidade atualizadorEntidade;
-
     @Autowired
     @InjectMocks
     private ClienteService clienteService;
 
+    private TestAuxiliaresClienteService testAuxiliaresClienteService;
+
     @BeforeEach
     void setup() {
         MockitoAnnotations.openMocks(this);
+        testAuxiliaresClienteService = new TestAuxiliaresClienteService(clienteRepository, documentValidator, clienteService);
     }
 
     @Test
     @DisplayName("Sucesso ao salvar um Cliente.")
     void saveCase1() {
-        Cliente cliente = createCliente(1L, "gemeoslemes", "375.243.170-93",
+        Cliente cliente = testAuxiliaresClienteService.createCliente(1L, "gemeoslemes", "375.243.170-93",
                 "gemeos@tgid.com", new BigDecimal(100), true);
-        mockDocumentValidator(cliente);
+        testAuxiliaresClienteService.mockDocumentValidator(cliente);
         cliente.setCpf("37524317093");
-        verifySaveAndFindMethods(cliente);
+        testAuxiliaresClienteService.verifySaveAndFindMethods(cliente);
     }
 
     @Test
     @DisplayName("Lançando exceção(CpfUniqueException) caso CPF já esteja cadastrado no DB.")
     void saveCase2() {
-        Cliente cliente = createCliente(1L, "gemeoslemes", "375.243.170-93",
+        Cliente cliente = testAuxiliaresClienteService.createCliente(1L, "gemeoslemes", "375.243.170-93",
                 "gemeos@tgid.com", new BigDecimal(100), true);
-        mockDocumentValidator(cliente);
+        testAuxiliaresClienteService.mockDocumentValidator(cliente);
         cliente.setCpf("37524317093");
-        verifySaveAndFindMethods(cliente);
+        testAuxiliaresClienteService.verifySaveAndFindMethods(cliente);
 
-        Cliente cliente2 = createCliente(2L, "viniciuslemes10", "375.243.170-93",
+        Cliente cliente2 = testAuxiliaresClienteService.createCliente(2L, "viniciuslemes10", "375.243.170-93",
                 "vinicius@tgid.com", new BigDecimal(1000), true);
-        mockDocumentValidator(cliente2);
-        simulateFindByCpf(cliente2);
+        testAuxiliaresClienteService.mockDocumentValidator(cliente2);
+        testAuxiliaresClienteService.simulateFindByCpf(cliente2);
 
         CpfUniqueException exception = new CpfUniqueException();
-        assertThrowsException(exception, cliente2);
-        verifySaveNeverCalled(cliente2);
+        testAuxiliaresClienteService.assertThrowsException(exception, cliente2);
+        testAuxiliaresClienteService.verifySaveNeverCalled(cliente2);
     }
 
     @Test
     @DisplayName("Lançando exceção(EmailUniqueException) caso email já esteja cadastrado no DB.")
     void saveCase3() {
-        Cliente cliente = createCliente(1L, "gemeoslemes", "375.243.170-93",
+        Cliente cliente = testAuxiliaresClienteService.createCliente(1L, "gemeoslemes", "375.243.170-93",
                 "gemeos@tgid.com", new BigDecimal(100), true);
-        mockDocumentValidator(cliente);
+        testAuxiliaresClienteService.mockDocumentValidator(cliente);
         cliente.setCpf("37524317093");
-        verifySaveAndFindMethods(cliente);
+        testAuxiliaresClienteService.verifySaveAndFindMethods(cliente);
 
-        Cliente cliente2 = createCliente(2L, "viniciuslemes10", "570.599.380-36",
+        Cliente cliente2 = testAuxiliaresClienteService.createCliente(2L, "viniciuslemes10", "570.599.380-36",
                 "gemeos@tgid.com", new BigDecimal(1000), true);
-        mockDocumentValidator(cliente2);
+        testAuxiliaresClienteService.mockDocumentValidator(cliente2);
         cliente2.setCpf("57059938036");
-        simulateFindByEmial(cliente2);
+        testAuxiliaresClienteService.simulateFindByEmial(cliente2);
 
         EmailUniqueException exception = new EmailUniqueException();
-        assertThrowsException(exception, cliente2);
-        verifySaveNeverCalled(cliente2);
-    }
-
-    private Cliente createCliente(Long id, String username, String cpf, String email, BigDecimal saldo, boolean status) {
-        return new Cliente(id, username, cpf, email, saldo, status);
-    }
-
-    private void mockDocumentValidator(Cliente cliente) {
-        String cpf = cliente.getCpf().replaceAll("[.-]", "");
-        when(documentValidator.isValid(cliente.getCpf(), "cliente")).thenReturn(cpf);
-    }
-
-    private void simulateFindByCpf(Cliente cliente) {
-        when(clienteRepository.findByCpf(cliente.getCpf())).thenReturn(cliente);
-    }
-
-    private void simulateFindByEmial(Cliente cliente) {
-        when(clienteRepository.findByEmail(cliente.getEmail())).thenReturn(cliente);
-    }
-
-    private void assertThrowsException(Exception exception, Cliente cliente) {
-        Assertions.assertThrows(exception.getClass(), () -> {
-            if(exception instanceof CpfUniqueException) {
-                clienteService.isCpfPresent(cliente);
-            } else if (exception instanceof EmailUniqueException) {
-                clienteService.isEmailPresent(cliente);
-            }
-        });
-    }
-
-    private void verifySaveNeverCalled(Cliente cliente) {
-        verify(clienteRepository, never()).save(cliente);
-    }
-
-    private void verifySaveAndFindMethods(Cliente cliente) {
-        clienteService.isCpfPresent(cliente);
-        verifyFindCpfClient(cliente);
-        clienteService.isEmailPresent(cliente);
-        verifyFindEmailClient(cliente);
-        clienteService.save(cliente);
-        verifySaveClient(cliente);
-    }
-
-    private void verifySaveClient(Cliente cliente) {
-        verify(clienteRepository, times(1)).save(cliente);
-    }
-
-    private void verifyFindEmailClient(Cliente cliente) {
-        verify(clienteRepository, times(1)).findByEmail(cliente.getEmail());
-    }
-
-    private void verifyFindCpfClient(Cliente cliente) {
-        verify(clienteRepository, times(1)).findByCpf(cliente.getCpf());
+        testAuxiliaresClienteService.assertThrowsException(exception, cliente2);
+        testAuxiliaresClienteService.verifySaveNeverCalled(cliente2);
     }
 }
--- src/test/java/com/tigd/api/service/TestAuxiliaresClienteService.java
+++ src/test/java/com/tigd/api/service/TestAuxiliaresClienteService.java
@@ -1,2 +1,77 @@
-package com.tigd.api.service;public class TestAuxiliaresClienteService {
+package com.tigd.api.service;
+
+import com.tigd.api.domain.Cliente;
+import com.tigd.api.exceptions.CpfUniqueException;
+import com.tigd.api.exceptions.EmailUniqueException;
+import com.tigd.api.repository.ClienteRepository;
+import com.tigd.api.validators.DocumentValidator;
+import org.junit.jupiter.api.Assertions;
+
+import java.math.BigDecimal;
+
+import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.times;
+
+public class TestAuxiliaresClienteService {
+    private final ClienteRepository clienteRepository;
+    private final DocumentValidator documentValidator;
+    private final ClienteService clienteService;
+
+    public TestAuxiliaresClienteService(ClienteRepository clienteRepository, DocumentValidator documentValidator, ClienteService clienteService) {
+        this.clienteRepository = clienteRepository;
+        this.documentValidator = documentValidator;
+        this.clienteService = clienteService;
+    }
+
+    protected void verifySaveNeverCalled(Cliente cliente) {
+        verify(clienteRepository, never()).save(cliente);
+    }
+
+    protected void verifySaveAndFindMethods(Cliente cliente) {
+        clienteService.isCpfPresent(cliente);
+        verifyFindCpfClient(cliente);
+        clienteService.isEmailPresent(cliente);
+        verifyFindEmailClient(cliente);
+        clienteService.save(cliente);
+        verifySaveClient(cliente);
+    }
+
+    protected Cliente createCliente(Long id, String username, String cpf, String email, BigDecimal saldo, boolean status) {
+        return new Cliente(id, username, cpf, email, saldo, status);
+    }
+
+    protected void mockDocumentValidator(Cliente cliente) {
+        String cpf = cliente.getCpf().replaceAll("[.-]", "");
+        when(documentValidator.isValid(cliente.getCpf(), "cliente")).thenReturn(cpf);
+    }
+
+    protected void simulateFindByCpf(Cliente cliente) {
+        when(clienteRepository.findByCpf(cliente.getCpf())).thenReturn(cliente);
+    }
+
+    protected void simulateFindByEmial(Cliente cliente) {
+        when(clienteRepository.findByEmail(cliente.getEmail())).thenReturn(cliente);
+    }
+
+    protected void assertThrowsException(Exception exception, Cliente cliente) {
+        Assertions.assertThrows(exception.getClass(), () -> {
+            if(exception instanceof CpfUniqueException) {
+                clienteService.isCpfPresent(cliente);
+            } else if (exception instanceof EmailUniqueException) {
+                clienteService.isEmailPresent(cliente);
+            }
+        });
+    }
+
+    protected void verifySaveClient(Cliente cliente) {
+        verify(clienteRepository, times(1)).save(cliente);
+    }
+
+    protected void verifyFindEmailClient(Cliente cliente) {
+        verify(clienteRepository, times(1)).findByEmail(cliente.getEmail());
+    }
+
+    protected void verifyFindCpfClient(Cliente cliente) {
+        verify(clienteRepository, times(1)).findByCpf(cliente.getCpf());
+    }
 }
